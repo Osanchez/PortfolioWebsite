d0c16dbf2d5a891f5d4a0534274f02d40ae9bec7
Omar Sanchez <osanchez@umass.edu>
2019-01-14 03:33:03 +0000

    fixed some wsgi errors

--- SpotiQueue/wsgi.py
+++ SpotiQueue/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/home/osanchez/SpotiQueue')
+sys.path.append('/home/osanchez/SpotiQueue/SpotiQueue')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SpotiQueue.settings')
 
